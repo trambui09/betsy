bf1170829bc720f8d55258a84a93d9c6aee61662
gessica <gessicamohr18@gmail.com>
2020-11-17 21:22:08 -0800

    created content on seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,72 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# #   Character.create(name: 'Luke', movie: movies.first)
+merchants = [
+    {
+        username: "merryx",
+        email: "merryx@gmail.com"
+    },
+    {
+        username: "hohoho",
+        email: "santa@gmail.com"
+    },
+    {
+        username: "rednose",
+        email: "rednose@gmail.com"
+    }
+]
+
+merchants.each do |merchant|
+  Merchant.create(merchant)
+end
+
+products = [
+    {
+        name: "tree",
+        price: 100.00,
+        merchant_id: (Merchant.find_by username: "hohoho").id
+    },
+    {
+        name: "sugar cookies",
+        price: 2.30,
+        merchant_id: (Merchant.find_by username: "merryx").id
+    },
+    {
+        name: "ornament",
+        price: 1.50,
+        merchant_id: (Merchant.find_by username: "rednose").id
+    }
+]
+
+products.each do |product|
+  Product.create(product)
+end
+
+
+
+categories = [
+    {
+        name: "decor"
+    },
+    {
+        name: "food"
+    },
+    {
+        name: "gifts"
+    }
+]
+
+categories.each do |category|
+  Category.create(category)
+end
+
+
+
+
+# cat1 = Category.create(name: "decor")
+# cat2 = Category.create(name: "food")
+# cat3 = Category.create(name: "gifts")
+
+
+
