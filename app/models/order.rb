44c35e9a7d64a407c7f7653501ea9d3b8badaeae
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-11-17 22:04:08 -0800

    added validations for Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
+  validates :name, :email, :address, :credit_card_num, :exp_data, :cvv, :zip_code,  presence: true
+  validates :cvv , :credit_card_num, numericality: { only_integer: true }
+  validates :cvv, length: { is: 3 }
+  validates :credit_card_num, length: { is: 16 }
+
 end
