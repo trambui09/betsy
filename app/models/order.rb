33de702c1bea6536a6c073d2985d8c1e0010dc38
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-11-17 22:18:01 -0800

    updated validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,4 @@ class Order < ApplicationRecord
   validates :cvv , :credit_card_num, numericality: { only_integer: true }
   validates :cvv, length: { is: 3 }
   validates :credit_card_num, length: { is: 16 }
-
 end
