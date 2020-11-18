7fa1b3a16650732d24fdc7b83bcf7c4ad930b94f
Tram Bui <pbui17@cmc.edu>
2020-11-17 20:11:30 -0600

    update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :products
+  resources :merchants
+  resources :order_items, only: [:delete, :update]
+  resources :orders
+  resources :categories
 end
