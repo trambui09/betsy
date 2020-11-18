3c227d4a1bd2702d5f254782ebd1a77855db2f5d
Tram Bui <pbui17@cmc.edu>
2020-11-18 14:18:00 -0600

    more tests for category model

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -10,6 +10,22 @@ describe Category do
   end
 
   describe "relations" do
+    it "can have many products" do
+      new_category.save
+      start_count = new_category.products.count
+
+      products = [products(:product_one), products(:product_two), products(:product_three)]
+      products.each do |product|
+        new_category.products << product
+      end
+
+      new_category.products.each do |product|
+        expect(product).must_be_instance_of Product
+      end
+
+      expect(new_category.products.count).must_equal start_count + 3
+
+    end
 
   end
 
@@ -20,7 +36,7 @@ describe Category do
 
     it "has to have a name" do
       new_category.name = nil
-      
+
       expect(new_category.valid?).must_equal false
       expect(new_category.errors.messages).must_include :name
       expect(new_category.errors.messages[:name]).must_equal ["can't be blank"]
