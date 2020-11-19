6ca8ec7a41b63d2bf06e87898c3c9835308f859a
gessica <gessicamohr18@gmail.com>
2020-11-18 17:32:30 -0800

    added methods and tests to cagetories_controller, tests passing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,33 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+    if @category.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def edit
+    @category = Category.find_by(id: params[:id])
+    if @category.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+
+  end
+
+
+
+  private
+  def category_params
+    return params.require(:category).permit(:category_id, :name)
+  end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,66 @@
 require "test_helper"
 
 describe CategoriesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "responds with success when there are many categories saved" do
+      # Arrange
+      Category.create name: "food"
+      # Act
+      get categories_path
+      # Assert
+      must_respond_with :success
+    end
+
+    it "responds with success when there are no categories saved" do
+      # Arrange
+
+      # Act
+      get categories_path
+      # Assert
+      must_respond_with :success
+    end
+  end
+
+
+  describe "show" do
+    # Arrange
+    before do
+      @category = Category.create(name: "food")
+    end
+    it "responds with success when showing an existing valid category" do
+      # # Arrange
+
+      id = @category.id
+      # Act
+
+      get categories_path(@category.id)
+
+      # Assert
+      must_respond_with :success
+
+    end
+
+    it "responds with 404 with an invalid category id" do
+      # Act
+      get category_path(-1)
+      # Assert
+      must_respond_with :not_found
+    end
+  end
+
+  describe "edit" do
+    before do
+      @category_1 = Driver.create(name: "food")
+    end
+    it "responds with success when getting the edit page for an existing, valid category" do
+
+      # Act
+
+      get edit_category_path(@category_1)
+
+      # Assert
+      must_respond_with :success
+    end
+
+  end
 end
