b06e04d4a5ccc62bc87efa819dd44ed06d285709
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-11-18 17:22:52 -0800

    Working on Order controller tests, created index/show pages for Order

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 describe OrdersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "will redirect when a user tries to get the page and isn't logged in" do
+      get orders_path
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+
+    end
+  end
 end
