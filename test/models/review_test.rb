5f27efd8d42b6f286c513cc5486cd78220fd6674
Tram Bui <pbui17@cmc.edu>
2020-11-20 10:37:35 -0600

    added tests to reviews model

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe Review do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @product = products(:product_one)
+  end
+  let (:new_review) {
+    Review.new(rating: 5,
+               description: "great overall",
+               product_id: @product.id)
+  }
+
+  it "can instantiated a review" do
+    expect(new_review.valid?).must_equal true
+  end
+
+  it "responds to its fields" do
+    new_review.save
+    review = Review.first
+
+    [:rating, :description].each do |field|
+      expect(review).must_respond_to field
+    end
+  end
+
+  describe "validations" do
+
+  end
+
+  describe "relations" do
+
+  end
+  
 end
