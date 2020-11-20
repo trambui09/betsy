f675fbf3c63845e3ae7f23995428122d88e102c0
Tram Bui <pbui17@cmc.edu>
2020-11-20 10:27:08 -0600

    added relationship and validation to review model

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :product
+
+  validates :rating, presence: true, inclusion: { in: 1..5,
+                                                 message: "rating must be between 1-5"}
+
+end
