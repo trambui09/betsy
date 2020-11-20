1a2672476bd22c2f0767515ffc436e7669c2e9d7
Tram Bui <pbui17@cmc.edu>
2020-11-20 10:46:28 -0600

    added more reviews relations tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -24,11 +24,36 @@ describe Review do
   end
 
   describe "validations" do
+    it "must have a rating between 1-5" do
+      new_review.rating = 7
+
+      expect(new_review.valid?).must_equal false
+      expect(new_review.errors.messages).must_include :rating
+      expect(new_review.errors.messages[:rating]).must_equal ["rating must be between 1-5"]
+
+    end
 
   end
 
   describe "relations" do
+    before do
+      @review_1 = Review.new(rating: 4,
+                             description: "Good enough")
+    end
+
+    it "can set the product using Product" do
+      @review_1.product = @product
+
+      expect(@review_1.product_id).must_equal @product.id
+    end
+
+    it "can set the product using product_id" do
+
+      @review_1.product_id = @product.id
+
+      expect(@review_1.product).must_equal @product
+    end
 
   end
-  
+
 end
