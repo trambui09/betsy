38dabf67c8ee0de31a2b8b61a4bf3d010d1eaeb9
Tram Bui <pbui17@cmc.edu>
2020-11-20 14:16:50 -0600

    added tests to merchant model

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 describe Merchant do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:new_merchant) {
+    Merchant.new(provider: "github",
+                 uid: 111,
+                 email: "testing@gmail.com",
+                 username: "testingname")
+  }
+
+  it "can instantiated a new merchant with the right fields" do
+    expect(new_merchant.valid?).must_equal true
+  end
+
+  it "can respond to all the fields" do
+    new_merchant.save
+    merchant = Merchant.first
+
+    [:provider, :uid, :email, :username].each do |field|
+      expect(merchant).must_respond_to field
+    end
+  end
+
 end
