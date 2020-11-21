0d08fc60328a3a714333104af9c3b16f3fc26cf0
Tram Bui <pbui17@cmc.edu>
2020-11-21 11:37:45 -0600

    added custom font to betsy

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,15 @@ module Betsy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # adding the fonts
+    # config.assets.paths << Rails.root.join("assets", "fonts")
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    # Rails.application.config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    #
+    #
+    # Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf .otf)
+
   end
 end
