c1301ca7669033e0ce82b550bd54c6c1b73b6ffb
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-11-22 19:57:34 -0800

    validations all passing for OrderItem Model tests

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe OrderItem do
-  let(:new_order) {
+  let(:new_order_item) {
     OrderItem.new(quantity: 1,
                   product: products(:product_three),
                   order: orders(:cart_one))
@@ -10,15 +10,49 @@ describe OrderItem do
 
     it "can be instantiated with valid data" do
 
-      expect(new_order.valid?).must_equal true
+      expect(new_order_item.valid?).must_equal true
     end
   end
 
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "is valid when all fields are present" do
+    new_order_item = order_items(:ornament_items)
+    result = new_order_item.valid?
 
-  describe "relations" do
+    expect(result).must_equal true
+  end
+
+  it "is invalid when a quantity is not present" do
+    new_order_item = order_items(:cookie_items)
+    new_order_item.quantity = nil
+    result = new_order_item.valid?
+
+    expect(result).must_equal false
+  end
+
+  it "is invalid with a quantity of zero or less" do
+    new_order_item = order_items(:tree_items)
+    new_order_item.quantity = 0
+    result = new_order_item.valid?
+
+    expect(result).must_equal false
+  end
+
+  it "is is valid with a quantity of greater than zero" do
+    new_order_item = order_items(:ornament_items)
+    result = new_order_item.valid?
+
+    expect(result).must_equal  true
+  end
+
+  it "is invalid when the quantity is not an integer" do
+    new_order_item = order_items(:cookie_items)
+    new_order_item.quantity = 2.8
+
+    expect(new_order_item.valid?).must_equal false
+  end
+
+  describe "relationships" do
+
 
   end
 
