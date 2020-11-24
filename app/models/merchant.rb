b31d9f31ef81601cd1de2d80468e231456f1e596
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-11-23 23:51:05 -0800

    Created total_revenue and total_order_status methods inside Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,6 @@
 class Merchant < ApplicationRecord
   has_many :products
+  has_many :order_items, through: :products
 
   validates :username, :email, presence: true
   validates :uid, uniqueness: { scope: :provider,
@@ -15,5 +16,34 @@ class Merchant < ApplicationRecord
     # Note that the merchant has not been saved.
     # We'll choose to do the saving outside of this method
     return merchant
+  end
+
+  def total_revenue
+    merchant_products = self.products
+    total_revenue = 0
+
+    merchant_products.each do |products|
+      products.order_items.each do |item|
+        if item.order.status == "cancelled"
+          next
+        end
+        total_revenue += item.quantity * item.product.price
+      end
+    end
+    return total_revenue
+  end
+
+  def total_orders_by_status(status)
+    status_hash = Hash.new(0)
+    if status.nil?
+      return self.order_items.count
+    else
+      self.order_items.each do |order_item|
+        if order_item.order.status == status
+          status_hash[order_item.order.id] = status
+        end
+      end
+      return status_hash.values.count(status)
+    end
   end
 end
